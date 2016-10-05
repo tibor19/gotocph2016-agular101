404cb092b94c048363969bd500aa59d6e64956a2
Tiberiu Covaci <tibi@covaci.se>
2016-10-05 13:01:10 +0100

    Added support for http service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import {FormsModule}      from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { Route, RouterModule } from '@angular/router';
 import { AppComponent }   from './app.component';
 import { OneComponent }   from './one.component';
@@ -8,9 +9,10 @@ import { AnotherComponent }   from './another.component';
 import {AppService} from './app.service';
 import { routing }              from './app.routing';
 
+import 'rxjs/Rx';
 
 @NgModule({
-  imports:      [ BrowserModule, RouterModule, FormsModule, routing ],
+  imports:      [ BrowserModule, HttpModule, RouterModule, FormsModule, routing ],
   declarations: [ AppComponent, OneComponent, AnotherComponent ],
   providers: [AppService],
   bootstrap:    [ AppComponent ]
--- app/app.service.ts
+++ app/app.service.ts
@@ -1,11 +1,16 @@
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 
+@Injectable()
 export class AppService {
-    constructor(){
+    constructor(private _http: Http){
 
     }
 
     getName()  : Promise<string> {
-        return Promise.resolve("From Service: Tibi");
+        return this._http
+            .get('data.json')
+            .toPromise()
+            .then(r => r.json());
     }
 }
